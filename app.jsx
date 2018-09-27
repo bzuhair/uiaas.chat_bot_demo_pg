93f971eb52691a2189aaf66a74cfec713e5d756b
Balal Zuhair <balal@MacBook-Pro-4.local>
2018-09-27 14:21:15 -0700

    add react router, redux logger, redux store and actions

--- app.jsx
+++ app.jsx
@@ -1,1 +1,14 @@
-console.log('Hello world');
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import { Provider } from 'react-redux';
+import store from './src/store/store';
+import routes from './src/components/index';
+
+ReactDOM.render(
+  <div>
+    <Provider store={store}>
+      {routes}
+    </Provider>
+  </div>, document.querySelector('#app')
+);
---
+++ src/components/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+// ROUTES
+import { } from '../constants/routeConstants';
+
+// COMPONENTS
+
+
+const history = createBrowserHistory();
+history.listen((location) => {
+  // Use setTimeout to make sure this runs after React Router's own listener
+  setTimeout(() => {
+    // Keep default behavior of restoring scroll position when user:
+    // - clicked back button
+    // - clicked on a link that programmatically calls `history.goBack()`
+    // - manually changed the URL in the address bar (here we might want
+    // to scroll to top, but we can't differentiate it from the others)
+    if (location.action === 'POP') {
+      return;
+    }
+    // In all other cases, scroll to top
+    window.scrollTo(0, 0);
+    console.log('location', location);
+  });
+});
+
+// main routes are here, sub-routes are in their respective container components
+export default (
+  <Router history={history}>
+    {/* <Main> */}
+    <Switch>
+      <Route path="" component="" />
+    </Switch>
+    {/* </Main> */}
+  </Router>
+);
